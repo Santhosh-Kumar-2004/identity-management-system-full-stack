0eb0c0118e8bba33f4ae466887a5847fc3a2134c
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-11-26 18:20:21 +0530

    Creating the table in the users

--- database/database_schema.sql
+++ database/database_schema.sql
@@ -2,3 +2,9 @@ CREATE DATABASE user_management_dev;
 
 -- Using the DATABASE and Using it
 USE user_management_dev; 
+
+CREATE TABLE users(
+	id VARCHAR(36) PRIMARY KEY NOT NULL, 
+    name VARCHAR(255) NOT NULL, 
+    
+);
