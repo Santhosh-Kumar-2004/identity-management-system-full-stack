6cfbe9d4f69115909ffa62c7c48ee8e489e0aa40
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-11-26 18:50:04 +0530

    Updating the insert schema

--- database/database_schema.sql
+++ database/database_schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE users(
 DROP TABLE users;
 
 INSERT INTO users(id, name, email, password, role, created_at, updated_at)
-VALUES ('550e8400-e29b-41d4-a716-446655440000', 'santhosh', 'santhosh@gmail.com', 'hvjvjh', 'admin'	);
+VALUES ('550e8400-e29b-41d4-a716-446655440000', 'santhosh', 'santhosh@gmail.com', 'hvjvjh', 'admin');
 
 
 
