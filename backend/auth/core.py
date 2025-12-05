3b56ec75751a401e4823ecf29586f6934e352315
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-05 13:36:18 +0530

    Created the hashing and verifying passowrd logics.

--- backend/auth/core.py
+++ backend/auth/core.py
@@ -4,6 +4,13 @@ from jose import JWTError, jwt
 # pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
 # passowrd1 = pwd_context.hash("mysecretpassword1234")
 # verify = pwd_context.verify("mysecretpassword123", passowrd1)
 
