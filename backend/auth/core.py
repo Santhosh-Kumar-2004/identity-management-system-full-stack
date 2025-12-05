9f74af9407438436201f44c3e3964498f0c353c5
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-05 12:56:49 +0530

    Testing the argon2 hash method

--- backend/auth/core.py
+++ backend/auth/core.py
@@ -1,10 +1,10 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 passowrd1 = pwd_context.hash("mysecretpassword123")
-verify = pwd_context.verify(passowrd1, "mysecretpassword123")
+verify = pwd_context.verify("mysecretpassword123", passowrd1)
 
 print(passowrd1)
 print(verify)
