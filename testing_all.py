5d5c59ecba792c876a1f339801d05b78b48062e4
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-05 13:40:06 +0530

    Created the hashing logic on my own

--- testing_all.py
+++ testing_all.py
@@ -1,3 +1,9 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["argon2"], )
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+def hash_pwd(password: str) -> str:
+    pwd_context.hash(password)
+
+def verify_pwd(plain_pwd: str, hashed_pwd: str) -> bool:
+    pwd_context.verify(plain_pwd, hashed_pwd)
