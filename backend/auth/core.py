4e1fdcfa5ac66815a852079b48ff849e993c09eb
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-05 12:49:53 +0530

    Updated

--- backend/auth/core.py
+++ backend/auth/core.py
@@ -2,6 +2,6 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-passowrd1 = pwd_context.hash("Santhosh12136")
+passowrd1 = pwd_context.hash("Santhosh1")
 
 print(passowrd1)
