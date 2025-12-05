fac6d633de6f5d2d801f3ec1adef2b8696c5d257
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-05 13:05:00 +0530

    Testing completed and done knowing the stuffs.

--- backend/auth/core.py
+++ backend/auth/core.py
@@ -3,8 +3,8 @@ from passlib.context import CryptContext
 # pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
-passowrd1 = pwd_context.hash("mysecretpassword1234")
-verify = pwd_context.verify("mysecretpassword123", passowrd1)
+# passowrd1 = pwd_context.hash("mysecretpassword1234")
+# verify = pwd_context.verify("mysecretpassword123", passowrd1)
 
-print(f"Here is the Hashed passowrd - {passowrd1}")
-print(verify)
+# print(f"Here is the Hashed passowrd - {passowrd1}")
+# print(verify)
