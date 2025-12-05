02fab830f6af4bf08ce6ac9c3c129d2f494875d4
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-05 12:58:23 +0530

    Adding debugger prints

--- backend/auth/core.py
+++ backend/auth/core.py
@@ -6,5 +6,5 @@ pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 passowrd1 = pwd_context.hash("mysecretpassword1234")
 verify = pwd_context.verify("mysecretpassword123", passowrd1)
 
-print(passowrd1)
+print(f"Here is the Hashed passowrd - {passowrd1}")
 print(verify)
