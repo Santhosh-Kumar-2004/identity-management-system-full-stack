d9996b0e1d1e84044abc4e9a76eb8fb9c18c5a1c
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-06 10:19:30 +0530

    Testing file cleaned

--- testing_all.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
-
-def hash_pwd(password: str) -> str:
-    pwd_context.hash(password)
-
-def verify_pwd(plain_pwd: str, hashed_pwd: str) -> bool:
-    pwd_context.verify(plain_pwd, hashed_pwd)
