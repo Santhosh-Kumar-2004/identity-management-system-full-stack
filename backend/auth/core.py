fea2a9d5b210c01e9906781042fc10d896c4d0f9
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-09 22:35:51 +0530

    Changing the debugging stuff

--- backend/auth/core.py
+++ backend/auth/core.py
@@ -25,9 +25,9 @@ def create_token(data: dict, expires_time: timedelta | None= None):
     print(f"The data which copied {data_copy}")
 
     expire = datetime.now(tz=datetime.timezone.utc) + (expires_time or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
-    
+
     data_copy.update({"exp": expire})
-    print(jwt.encode(data_copy, SECURITY_KEY, algorithm=ALGORITHM))
+    print(data_copy, SECURITY_KEY, algorithm=ALGORITHM)
 
     return jwt.encode(data_copy, SECURITY_KEY, algorithm=ALGORITHM)
 
