26599a9cf2159996e480d2e2213fc81d946799c6
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-10 12:33:55 +0530

    Done the jwt creation successfully

--- testing_all.py
+++ testing_all.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from jose import jwt, JWTError
 
 def create_token(data: dict, expires_time: timedelta | None= None):
     data_copy = data.copy()
@@ -6,3 +7,5 @@ def create_token(data: dict, expires_time: timedelta | None= None):
     expire = datetime.now(tz=datetime.timezone.utc) + (expires_time or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
 
     data_copy.update({"exp": expire})
+
+    jwt.encode(data_copy, SECURITY_KEY, algorithm=ALGORITHM)
