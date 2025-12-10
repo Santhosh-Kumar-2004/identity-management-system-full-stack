011e973994c902f17b3a1e095e260d2fa4f3e209
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-10 12:36:02 +0530

    Iported the env stuufs libraries

--- testing_all.py
+++ testing_all.py
@@ -1,11 +1,16 @@
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 def create_token(data: dict, expires_time: timedelta | None= None):
     data_copy = data.copy()
 
-    expire = datetime.now(tz=datetime.timezone.utc) + (expires_time or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.now(tz=datetime.timezone.utc) + (expires_time or timedelta(minutes=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))
 
     data_copy.update({"exp": expire})
 
-    jwt.encode(data_copy, SECURITY_KEY, algorithm=ALGORITHM)
+    jwt.encode(data_copy, os.getenv("SECURITY_KEY"), algorithm=os.getenv("ALGORITHM"))
