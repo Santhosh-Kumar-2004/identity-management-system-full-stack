78716f4cc0f255c25cee9a8a4b055cbb306dce18
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-11 13:53:20 +0530

    Updated the retuning statement

--- backend/auth/core.py
+++ backend/auth/core.py
@@ -91,7 +91,7 @@ def get_current_user(
             status_code=status.HTTP_401_UNAUTHORIZED
         )
 
-    return ResponseUser.from(user)
+    return ResponseUser.model_validate(user)
 
 
 # passowrd1 = pwd_context.hash("mysecretpassword1234")
