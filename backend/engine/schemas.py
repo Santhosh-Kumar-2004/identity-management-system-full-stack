ef0450fbc63c17f3a64a26f670764b2d840fb264
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-11 13:11:49 +0530

    Created the Login user schma succesfully

--- backend/engine/schemas.py
+++ backend/engine/schemas.py
@@ -1,4 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
 from models import UserRole
+
+class LoginUser(BaseModel):
+    name: Optional[str] = None
+    email: EmailStr
+    password: str
+    role: Optional[UserRole] = UserRole.user
