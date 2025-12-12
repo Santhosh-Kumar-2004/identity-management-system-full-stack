6d6baf6a762189f43cf075d56e8a553f8914b112
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-12 12:06:28 +0530

    IMorted the pwd and token related logics from core.

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -4,3 +4,4 @@ from sqlalchemy.orm import Session
 from helper.db_helper import get_db
 from engine.models import User
 from engine.schemas import CreateUser, LoginUser, UpdateUser, ResponseUser
+from auth.core import hash_password, verify_password, create_token, decode_token
