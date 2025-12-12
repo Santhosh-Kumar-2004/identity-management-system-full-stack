b3355256178b05eb651a4d513a7f199908a6b494
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-12 10:52:25 +0530

    Testing the env variables

--- testing_all.py
+++ testing_all.py
@@ -3,5 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-acces_token = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+access_token = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 algorithm = os.getenv("ALGORITHM")
+
+print(access_token)
+print(algorithm)
