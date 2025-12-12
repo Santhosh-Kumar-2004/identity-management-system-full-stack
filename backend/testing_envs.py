8070a2be611a097ca762f05da005f6a7b37d7b19
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-12 11:49:55 +0530

    Checking the db url and it is wrong, So debugging it

--- backend/testing_envs.py
+++ backend/testing_envs.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 access_token = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 algorithm = os.getenv("ALGORITHM")
+db_url = os.getenv("DATABASE_URL")
 
 print(access_token)
 print(algorithm)
+print(db_url)
