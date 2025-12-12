007c1ea00ced8f66fe2558108accd46e9bc815bb
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-12 12:19:47 +0530

    Creatin the post endpoint for the def Register

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -8,3 +8,15 @@ from auth.core import hash_password, verify_password, create_token, decode_token
 from auth.core import get_current_user
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+
+@router.post("/register", response_model=ResponseUser, status_code=status.HTTP_201_CREATED)
+def register(
+    user: CreateUser,
+    db: Session = Depends(get_db)
+):
+    """Resgiter Endpoint
+
+    Args:
+        user (CreateUser Schema Used): Register endpoint used to create the new users.
+        db (Session will use get_db, optional): Which create a new session to use this endpoint. Defaults to Depends(get_db).
+    """
