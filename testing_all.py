63fc8e31bb18acb57d86260dc0a809d422a0013f
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-12 11:50:43 +0530

    Commenting the useless lines

--- testing_all.py
+++ testing_all.py
@@ -6,5 +6,5 @@ load_dotenv()
 access_token = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 algorithm = os.getenv("ALGORITHM")
 
-print(access_token)
-print(algorithm)
+# print(access_token)
+# print(algorithm)
