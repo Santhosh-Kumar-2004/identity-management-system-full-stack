b3fff7e3d57f1cd1c48542c0a9cf53e14cfdf2e2
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-17 13:28:20 +0530

    Imported the Engine and base from helper and models

--- backend/main.py
+++ backend/main.py
@@ -3,3 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from routers import users as user_routes
 from routers import admins as admin_routes
+
+from helper.db_helper import engine
+from engine.models import Base
