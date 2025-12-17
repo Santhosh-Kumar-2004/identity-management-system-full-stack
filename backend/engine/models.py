5944d5899777ec829a62020ac53f0c53229fdd80
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-17 13:48:37 +0530

    Updated the models .py file

--- backend/engine/models.py
+++ backend/engine/models.py
@@ -21,12 +21,12 @@ class UserRole(PyEnum):
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(String(36), primary_key=True, default= lambda: str(uuid.uuid4())),
-    name = Column(String(255), nullable=False),
-    email = Column(String(255), nullable=False, unique=True),
-    password = Column(String(255), nullable=False),
-    role = Column(SQLEnum(UserRole), nullable=False, default=UserRole.user),
-    is_logged = Column(Boolean, nullable=False, default=False),
+    id = Column(String(36), primary_key=True, default= lambda: str(uuid.uuid4()))
+    name = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    role = Column(SQLEnum(UserRole), nullable=False, default=UserRole.user)
+    is_logged = Column(Boolean, nullable=False, default=False)
 
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), nullable=False, onupdate=func.now())
