afec3f2db124c92a0df1242d2b8a0bb4e8306630
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-22 12:17:33 +0530

    Wrote a if condition on my own to check the token in the loacalStorage

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,9 +1,14 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Navigate } from "react-router";
+import { getToken } from "../api/api";
 
 const ProtectedRoute = ({ children }) => {
     const {loading, isAuthenticated }= useContext(AuthContext)
+
+    if (!getToken) {
+        return <Navigate to="/login" replace/>
+    }
 
     if (loading) {
         return <p>The App is Loading...</p>
