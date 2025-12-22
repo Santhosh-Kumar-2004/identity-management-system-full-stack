237c1884ca208b40e8a3add16eb4c963068b1b4d
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-22 11:56:49 +0530

    Adding all to the main App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,9 @@ import { AuthProvider } from './context/AuthContext.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
 )
