ce2e44ef8aa41d366ecd919861274f7f30656c6d
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-12-22 12:22:51 +0530

    IMporting the variables fro auth context

--- frontend/src/routes/AdminRoute.jsx
+++ frontend/src/routes/AdminRoute.jsx
@@ -1,3 +1,7 @@
 import { AuthContext } from "../context/AuthContext";
 import { Navigate } from "react-router";
 import { useContext } from "react";
+
+const AdminRoute = ({ children }) => {
+    const { loading, user, isAuthenticated } = useContext(AuthContext)
+}
