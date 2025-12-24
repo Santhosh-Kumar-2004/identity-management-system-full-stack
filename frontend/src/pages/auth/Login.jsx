d11d649cad11fd503e175f177304d2c9b2f44332
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2025-12-24 12:31:19 +0530

    Created the login function and creating the handlesubmit function

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,3 +1,18 @@
 import { useState, useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+
+const Login = () => {
+    const { login } = useContext(AuthContext);
+    const Navigate = useNavigate()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError("")
+        
+    }
+}
