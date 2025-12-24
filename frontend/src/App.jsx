5206a236e99e7a1fb37e90ef2bd39fdbb71111f2
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2025-12-24 12:43:11 +0530

    Commenting the imports of the app.jsx too

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router'
 
-import ProtectedRoute from './routes/ProtectedRoute'
-import AdminRoute from './routes/AdminRoute'
+// import ProtectedRoute from './routes/ProtectedRoute'
+// import AdminRoute from './routes/AdminRoute'
 
 import Login from './pages/auth/Login'
-import Register from './pages/auth/Register'
-import Home from './pages/public/Home'
-import AllUsers from './pages/admin/UserDetails'
-import Profile from './pages/user/Profile'
+// import Register from './pages/auth/Register'
+// import Home from './pages/public/Home'
+// import AllUsers from './pages/admin/UserDetails'
+// import Profile from './pages/user/Profile'
 
 
 function App() {
