d3173a61761e37e47cfc145f210cc51942e70412
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2025-12-24 12:54:11 +0530

    Created the new auth context file

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,3 +1,68 @@
-import { createContext } from "react";
+import { useState, useEffect } from "react"; 
+import { 
+    loginUser,
+    getCurrentUser,
+    setToken,
+    removeToken,
+} from "../api/api";
 
-export const AuthContext = createContext(null);
+// export const AuthContext = createContext(null)
+
+export const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null)
+
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+    const [loading, setLoading] = useState(true)
+
+    const login = async (email, password) => {
+        const response = loginUser({ email, password })
+
+        setToken(response.access_token)
+
+        const userData = getCurrentUser()
+
+        setUser(userData)
+        setIsAuthenticated(true)
+    }
+
+    const logout = () => {
+        removeToken()
+        setIsAuthenticated(false)
+        setUser(null)
+    }
+
+    useEffect(() => {
+        const loadUser = async () => {
+            try {
+                const userData = await getCurrentUser()
+                setUser(userData)
+                setIsAuthenticated(true)
+            } catch () {
+                setUser(null)
+                setIsAuthenticated(false)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        loadUser()
+    }, [])
+
+    return (
+        <AuthContext.Provider
+            value={{
+                user,
+                isAuthenticated,
+                login,
+                logout,
+                loading
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+
